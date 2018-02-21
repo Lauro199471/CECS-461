4605dd00d4b9a320e69e54bf6d667ef7dd9e6163
LauroCabral <Lauro199471@gmail.com>
2018-02-21 08:48:07 -0800

    Added a simple GPIO project

---
+++ Example_GPIO/gpioExample/gpioExample.sdk/lauroGPIOTesting/Debug/src/helloworld.d
@@ -0,0 +1,48 @@
+src/helloworld.o src/helloworld.d: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xgpio.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xgpio_l.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xgpio.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xgpio_l.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../lauroGPIOTesting_bsp/ps7_cortexa9_0/include/xparameters.h:
--- Example_GPIO/gpioExample/gpioExample.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ Example_GPIO/gpioExample/gpioExample.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2015 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2015.2 (win64) Build 1266856 Fri Jun 26 16:35:25 MDT 2015
---Date        : Tue Feb 20 16:29:05 2018
+--Date        : Wed Feb 21 07:40:55 2018
 --Host        : Lauro-Laptop running 64-bit major release  (build 9200)
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
@@ -33,7 +33,9 @@ entity design_1_wrapper is
     FIXED_IO_mio : inout STD_LOGIC_VECTOR ( 53 downto 0 );
     FIXED_IO_ps_clk : inout STD_LOGIC;
     FIXED_IO_ps_porb : inout STD_LOGIC;
-    FIXED_IO_ps_srstb : inout STD_LOGIC
+    FIXED_IO_ps_srstb : inout STD_LOGIC;
+    btns_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_4bits_tri_io : inout STD_LOGIC_VECTOR ( 3 downto 0 )
   );
 end design_1_wrapper;
 
@@ -60,9 +62,37 @@ architecture STRUCTURE of design_1_wrapper is
     FIXED_IO_ddr_vrp : inout STD_LOGIC;
     FIXED_IO_ps_srstb : inout STD_LOGIC;
     FIXED_IO_ps_clk : inout STD_LOGIC;
-    FIXED_IO_ps_porb : inout STD_LOGIC
+    FIXED_IO_ps_porb : inout STD_LOGIC;
+    btns_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_4bits_tri_i : in STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_4bits_tri_o : out STD_LOGIC_VECTOR ( 3 downto 0 );
+    leds_4bits_tri_t : out STD_LOGIC_VECTOR ( 3 downto 0 )
   );
   end component design_1;
+  component IOBUF is
+  port (
+    I : in STD_LOGIC;
+    O : out STD_LOGIC;
+    T : in STD_LOGIC;
+    IO : inout STD_LOGIC
+  );
+  end component IOBUF;
+  signal leds_4bits_tri_i_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal leds_4bits_tri_i_1 : STD_LOGIC_VECTOR ( 1 to 1 );
+  signal leds_4bits_tri_i_2 : STD_LOGIC_VECTOR ( 2 to 2 );
+  signal leds_4bits_tri_i_3 : STD_LOGIC_VECTOR ( 3 to 3 );
+  signal leds_4bits_tri_io_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal leds_4bits_tri_io_1 : STD_LOGIC_VECTOR ( 1 to 1 );
+  signal leds_4bits_tri_io_2 : STD_LOGIC_VECTOR ( 2 to 2 );
+  signal leds_4bits_tri_io_3 : STD_LOGIC_VECTOR ( 3 to 3 );
+  signal leds_4bits_tri_o_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal leds_4bits_tri_o_1 : STD_LOGIC_VECTOR ( 1 to 1 );
+  signal leds_4bits_tri_o_2 : STD_LOGIC_VECTOR ( 2 to 2 );
+  signal leds_4bits_tri_o_3 : STD_LOGIC_VECTOR ( 3 to 3 );
+  signal leds_4bits_tri_t_0 : STD_LOGIC_VECTOR ( 0 to 0 );
+  signal leds_4bits_tri_t_1 : STD_LOGIC_VECTOR ( 1 to 1 );
+  signal leds_4bits_tri_t_2 : STD_LOGIC_VECTOR ( 2 to 2 );
+  signal leds_4bits_tri_t_3 : STD_LOGIC_VECTOR ( 3 to 3 );
 begin
 design_1_i: component design_1
      port map (
@@ -86,6 +116,47 @@ design_1_i: component design_1
       FIXED_IO_mio(53 downto 0) => FIXED_IO_mio(53 downto 0),
       FIXED_IO_ps_clk => FIXED_IO_ps_clk,
       FIXED_IO_ps_porb => FIXED_IO_ps_porb,
-      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb
+      FIXED_IO_ps_srstb => FIXED_IO_ps_srstb,
+      btns_4bits_tri_i(3 downto 0) => btns_4bits_tri_i(3 downto 0),
+      leds_4bits_tri_i(3) => leds_4bits_tri_i_3(3),
+      leds_4bits_tri_i(2) => leds_4bits_tri_i_2(2),
+      leds_4bits_tri_i(1) => leds_4bits_tri_i_1(1),
+      leds_4bits_tri_i(0) => leds_4bits_tri_i_0(0),
+      leds_4bits_tri_o(3) => leds_4bits_tri_o_3(3),
+      leds_4bits_tri_o(2) => leds_4bits_tri_o_2(2),
+      leds_4bits_tri_o(1) => leds_4bits_tri_o_1(1),
+      leds_4bits_tri_o(0) => leds_4bits_tri_o_0(0),
+      leds_4bits_tri_t(3) => leds_4bits_tri_t_3(3),
+      leds_4bits_tri_t(2) => leds_4bits_tri_t_2(2),
+      leds_4bits_tri_t(1) => leds_4bits_tri_t_1(1),
+      leds_4bits_tri_t(0) => leds_4bits_tri_t_0(0)
+    );
+leds_4bits_tri_iobuf_0: component IOBUF
+     port map (
+      I => leds_4bits_tri_o_0(0),
+      IO => leds_4bits_tri_io(0),
+      O => leds_4bits_tri_i_0(0),
+      T => leds_4bits_tri_t_0(0)
+    );
+leds_4bits_tri_iobuf_1: component IOBUF
+     port map (
+      I => leds_4bits_tri_o_1(1),
+      IO => leds_4bits_tri_io(1),
+      O => leds_4bits_tri_i_1(1),
+      T => leds_4bits_tri_t_1(1)
+    );
+leds_4bits_tri_iobuf_2: component IOBUF
+     port map (
+      I => leds_4bits_tri_o_2(2),
+      IO => leds_4bits_tri_io(2),
+      O => leds_4bits_tri_i_2(2),
+      T => leds_4bits_tri_t_2(2)
+    );
+leds_4bits_tri_iobuf_3: component IOBUF
+     port map (
+      I => leds_4bits_tri_o_3(3),
+      IO => leds_4bits_tri_io(3),
+      O => leds_4bits_tri_i_3(3),
+      T => leds_4bits_tri_t_3(3)
     );
 end STRUCTURE;
