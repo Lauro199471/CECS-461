b4dd700bcfd2e9fa692f725ec7d57ad9a7b8e86c
LauroCabral <Lauro199471@gmail.com>
2018-03-17 15:20:05 -0700

    Added cool things

---
+++ EmptyProject/project_1.sdk/helloworld/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.d: ../src/platform.c \
+ ../../helloworld_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../helloworld_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../helloworld_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../helloworld_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../helloworld_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../helloworld_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../helloworld_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../helloworld_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
